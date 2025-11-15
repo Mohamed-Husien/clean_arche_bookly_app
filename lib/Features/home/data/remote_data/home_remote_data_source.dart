40d4f91fd5d1409bb16e8d2d27001d4c678505c3
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-11-15 18:06:04 +0200

    create home remote data source

---
+++ lib/Features/home/data/remote_data/home_remote_data_source.dart
@@ -0,0 +1,6 @@
+import 'package:clean_arche_bookly_app/Features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
