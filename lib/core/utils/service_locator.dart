112af00518091b0633a2782cd65ff6e374095692
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-11-16 08:21:13 +0200

    create service locator

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,24 @@
+import 'package:clean_arche_bookly_app/Features/home/data/data_sources/home_local_data_source.dart';
+import 'package:clean_arche_bookly_app/Features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:clean_arche_bookly_app/Features/home/data/repo/home_repo_impl.dart';
+import 'package:clean_arche_bookly_app/core/utils/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  getIt.registerSingleton<ApiService>(
+    ApiService(
+      Dio(),
+    ),
+  );
+  getIt.registerSingleton<HomeRepoImpl>(
+    HomeRepoImpl(
+      homeLocalDataSource: HomeLocalDataSourceImpl(),
+      homeRemoteDataSource: HomeRemoteDataSourceImpl(
+        apiService: getIt.get<ApiService>(),
+      ),
+    ),
+  );
+}
