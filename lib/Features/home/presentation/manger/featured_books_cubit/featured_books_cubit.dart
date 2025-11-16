2da9b2f24b5bfec480c0df3216799b0989bb23d4
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-11-16 07:17:55 +0200

    featured books cubit done

--- lib/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,28 @@
 import 'package:bloc/bloc.dart';
 import 'package:clean_arche_bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:clean_arche_bookly_app/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:meta/meta.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.fetchFeaturedBooksUseCase)
+      : super(FeaturedBooksInitial());
+  final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await fetchFeaturedBooksUseCase.call();
+    result.fold(
+      (failure) {
+        emit(
+          FeaturedBooksFailure(errMessage: failure.errorMessage),
+        );
+      },
+      (books) {
+        emit(
+          FeaturedBooksSuccess(booksList: books),
+        );
+      },
+    );
+  }
 }
