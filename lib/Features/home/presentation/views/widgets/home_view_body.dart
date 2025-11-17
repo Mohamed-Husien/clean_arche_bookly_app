225be437bd75b9c54ca008d2d8fb9be147931345
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-11-17 14:33:01 +0200

    refactor to make scroll controller on custom scroll view instead of listView

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,39 +1,76 @@
 import 'package:clean_arche_bookly_app/Features/home/presentation/views/widgets/custom_best_seller_list_view_bloc_builder.dart';
 import 'package:clean_arche_bookly_app/Features/home/presentation/views/widgets/custom_featured_books_bloc_builder.dart';
+import 'package:clean_arche_bookly_app/Features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart';
 import 'package:clean_arche_bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'custom_app_bar.dart';
 
-class HomeViewBody extends StatelessWidget {
+class HomeViewBody extends StatefulWidget {
   const HomeViewBody({super.key});
 
   @override
+  State<HomeViewBody> createState() => _HomeViewBodyState();
+}
+
+class _HomeViewBodyState extends State<HomeViewBody> {
+  late ScrollController _scrollController;
+  int nextPage = 1;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_onScroll);
+  }
+
+  void _onScroll() async {
+    if (!_scrollController.hasClients || !mounted) return;
+
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.offset;
+    final threshold = maxScroll * 0.9;
+
+    if (currentScroll >= threshold) {
+      if (!isLoading) {
+        isLoading = true;
+
+        await BlocProvider.of<NewestBooksCubit>(context)
+            .fetchNewestBooks(pageNumber: nextPage++);
+
+        isLoading = false;
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      slivers: [
+    return CustomScrollView(
+      controller: _scrollController,
+      slivers: const [
         SliverToBoxAdapter(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 30,
-                  ),
-                  child: CustomAppBar()),
-              CustomFeaturedBooksBlocConsumer(),
-              SizedBox(
-                height: 50,
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: CustomAppBar(),
               ),
+              CustomFeaturedBooksBlocConsumer(),
+              SizedBox(height: 50),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 30),
-                child: Text(
-                  'Newest Books',
-                  style: Styles.textStyle18,
-                ),
+                child: Text('Newest Books', style: Styles.textStyle18),
               ),
-              SizedBox(
-                height: 20,
-              ),
+              SizedBox(height: 20),
             ],
           ),
         ),
@@ -42,6 +79,9 @@ class HomeViewBody extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 30),
             child: CustomBestSellerListViewBlocBuilder(),
           ),
+        ),
+        SliverToBoxAdapter(
+          child: SizedBox(height: 40),
         ),
       ],
     );
