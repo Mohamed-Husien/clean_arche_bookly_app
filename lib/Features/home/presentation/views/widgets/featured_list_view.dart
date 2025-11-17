323a46c585cb01ebb8539bdd2bc4d6adbf4e951d
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-11-17 10:26:23 +0200

    trigger featured cubit when reach 70% of list index

--- lib/Features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,33 +1,79 @@
 import 'package:clean_arche_bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:clean_arche_bookly_app/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'custom_book_item.dart';
 
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key, required this.books});
+class FeaturedBooksListView extends StatefulWidget {
+  const FeaturedBooksListView({
+    super.key,
+    required this.books,
+  });
+
   final List<BookEntity> books;
+
+  /// callback to call fetchFeaturedBooks()
+
+  @override
+  State<FeaturedBooksListView> createState() => _FeaturedBooksListViewState();
+}
+
+class _FeaturedBooksListViewState extends State<FeaturedBooksListView> {
+  late ScrollController _scrollController;
+  int nextPage = 1;
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_onScroll);
+  }
+
+  void _onScroll() {
+    if (!_scrollController.hasClients) return;
+
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.offset;
+    final threshold = maxScroll * 0.7;
+
+    if (currentScroll >= threshold) {
+      BlocProvider.of<FeaturedBooksCubit>(context)
+          .fetchFeaturedBooks(pageNumber: nextPage++);
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * .3,
       child: ListView.builder(
-          itemCount: books.length,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            final imageUrl = books[index].image;
-            const fallbackUrl =
-                'https://basecamplive.com/wp-content/uploads/2023/02/books.jpeg';
+        controller: _scrollController,
+        scrollDirection: Axis.horizontal,
+        itemCount: widget.books.length,
+        itemBuilder: (context, index) {
+          final imageUrl = widget.books[index].image;
+          const fallbackUrl =
+              'https://basecamplive.com/wp-content/uploads/2023/02/books.jpeg';
 
-            final safeImageUrl = (imageUrl != null && imageUrl.isNotEmpty)
-                ? imageUrl
-                : fallbackUrl;
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: CustomBookImage(
-                image: safeImageUrl,
-              ),
-            );
-          }),
+          final safeImageUrl = (imageUrl != null && imageUrl.isNotEmpty)
+              ? imageUrl
+              : fallbackUrl;
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: CustomBookImage(
+              image: safeImageUrl,
+            ),
+          );
+        },
+      ),
     );
   }
 }
