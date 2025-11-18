8087da641d716e03da15b4cd6e9819143681ce29
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-11-18 09:43:41 +0200

    fetch from hive

--- lib/Features/home/data/data_sources/home_local_data_source.dart
+++ lib/Features/home/data/data_sources/home_local_data_source.dart
@@ -5,6 +5,7 @@ import 'package:hive/hive.dart';
 abstract class HomeLocalDataSource {
   List<BookEntity> fetchFeaturedBooks({int pageNumber = 0});
   List<BookEntity> fetchNewestBooks({int pageNumber = 0});
+  List<BookEntity> fetchSimilarBooks({int pageNumber = 0});
 }
 
 class HomeLocalDataSourceImpl extends HomeLocalDataSource {
@@ -25,7 +26,19 @@ class HomeLocalDataSourceImpl extends HomeLocalDataSource {
     int startIndex = pageNumber * 10;
     int endIndex = (pageNumber + 1) * 10;
     var box = Hive.box<BookEntity>(kNewestBooks);
-     int length = box.values.length;
+    int length = box.values.length;
+    if (startIndex >= length || endIndex > length) {
+      return [];
+    }
+    return box.values.toList();
+  }
+
+  @override
+  List<BookEntity> fetchSimilarBooks({int pageNumber = 0}) {
+    int startIndex = pageNumber * 10;
+    int endIndex = (pageNumber + 1) * 10;
+    var box = Hive.box<BookEntity>(kSimilarBooks);
+    int length = box.values.length;
     if (startIndex >= length || endIndex > length) {
       return [];
     }
